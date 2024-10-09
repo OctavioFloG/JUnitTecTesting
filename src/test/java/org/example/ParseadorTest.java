5d1f5eb728283b3f4e52d43adb6573faecdba8dd
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2024-10-08 23:02:56 -0600

    Mas pruebas unitarias

--- src/test/java/org/example/ParseadorTest.java
+++ src/test/java/org/example/ParseadorTest.java
@@ -38,6 +38,8 @@ public class ParseadorTest {
         assertEquals(3, resultado);
     }
 
+    EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+
     @Test
     public void obtenerOperadorSuma() {
 
@@ -46,5 +48,35 @@ public class ParseadorTest {
         String operador = parseador.obtenerOperador(ecuacion2);
 
         assertEquals("+", operador);
+    }
+
+    @Test
+    public void solucionaEcuacionConMenos() {
+
+        Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+
+        Double valueExpected = 0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConMas() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+
+        Double valueExpected = -0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConParte3Mayor0() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 10");
+
+        Double valueExpected = 4.5;
+
+        assertEquals(valueExpected, result);
     }
 }
